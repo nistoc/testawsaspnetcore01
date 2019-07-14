c6740b93b90cb4dc8bd55241c31c5b2e8f37e10e
Nikita Temnikov <nistoc@gmail.com>
2019-07-14 17:45:07 +0300

    lil change

--- MyCoreTest/MyCoreTest/Startup.cs
+++ MyCoreTest/MyCoreTest/Startup.cs
@@ -48,7 +48,7 @@ namespace MyCoreTest
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
